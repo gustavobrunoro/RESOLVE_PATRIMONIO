b7668168432eaf972cdf299bf5f3fa5104b15680
gustavo.meira <gustavobrunoro@hotmail.com>
2019-05-20 09:29:06 -0300

    Atualização Diversa

--- app/src/main/java/com/resolve/gustavobrunoromeira/resolve_patrimonio/API/APIResolve.java
+++ app/src/main/java/com/resolve/gustavobrunoromeira/resolve_patrimonio/API/APIResolve.java
@@ -2,8 +2,8 @@ package com.resolve.gustavobrunoromeira.resolve_patrimonio.API;
 
 public class APIResolve {
 
-    //public static String URL_PRINCIPAL = "http://172.16.1.149:80";
-    public static String URL_PRINCIPAL = "http://192.168.1.102:80";
+    public static String URL_PRINCIPAL = "http://172.16.1.104:80";
+    //public static String URL_PRINCIPAL = "http://192.168.1.102:80";
     public static String URL_BASE = URL_PRINCIPAL + "/api/";
 
 }
